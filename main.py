22dd559f61238ded4652d398ba0a2e05bd3b5323
Mathman <>
2021-04-20 10:43:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from random import randint
+print("Enter your first word:")
+first_word = input().lower()
+print("Enter your second word:")
+second_word = input().lower()
+print("Enter your third word:")
+third_word = input().lower()
+password = first_word + second_word + third_word
+for letter in password:
+  if letter == a:
+    random_number = randint(33,37)
